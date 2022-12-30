2d4283f3eadae4ce2e972a1ca33c4bc193879550
Hojin Kim <miu.null@gmail.com>
2022-12-30 19:39:11 +0900

    머지 전 커밋

--- routes/driver.routes.js
+++ routes/driver.routes.js
@@ -1,11 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-// const DriverController = require('../controllers/driver.controller');
-// const driverController = new DriverController();
+const DriverController = require("../controllers/driver.controller");
+const driverController = new DriverController();
 
-router.post('/driver/signup', (req, res) => {
-    
-})
+router.post("/driver/signup", (req, res) => {});
 
+// 기사 (사장) 전체조회하기 / 메인페이지
+router.get("/drivers", (req, res) => {
+    // 보낼 데이터베이스를 선택 /
+    //
+    //     res.json({ driver : image })
+    // const Drivers = require("")
+});
 module.exports = router;
