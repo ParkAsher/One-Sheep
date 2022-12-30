e49074e95e36d53775c82ad3043005ad686fa33f
Jin Bae <jinbae00@gmail.com>
2022-12-30 19:39:14 +0900

    master 브랜치 풀 전

---
+++ __tests__/unit/order.repository.unit.spec.js
@@ -0,0 +1,15 @@
+const OrderRepositoryDepInj = require('../../repositories/order.repository.depInj')
+
+
+let mockOrderModel = {
+    create: jest.fn(),
+}
+
+let orderRepository = new OrderRepositoryDepInj(mockOrderModel)
+
+describe('3계층 아키텍처 Order repository 유닛 테스트', () => {
+    beforeEach(() => {
+        
+    })
+
+})
