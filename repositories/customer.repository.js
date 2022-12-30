6a736fb12f02453defa16ca61946038cb03d70aa
ParkAsher <devasherpark@naver.com>
2022-12-30 19:11:00 +0900

    feat: customer 계층 구조 분리

---
+++ repositories/customer.repository.js
@@ -0,0 +1,8 @@
+class CustomerRepository {
+    constructor(CustomerModel) {
+        // 생성자 주입
+        this.customerModel = CustomerModel;
+    }
+}
+
+module.exports = CustomerRepository;
--- routes/customer.routes.js
+++ routes/customer.routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-// const CustomerController = require('../controllers/customer.controller');
-// const customerController = new CustomerController();
+// controllers
+const CustomerController = require('../controllers/customer.controller.js');
+const customerController = new CustomerController();
 
-router.post('/customer/signup', (req, res) => {
-    
-})
+// 고객 회원가입
+router.post('/customer/signup', customerController.signUp);
+
+module.exports = router;
