da075255455f1d9494128eea8ca526c82eed16ae
ParkAsher <devasherpark@naver.com>
2023-01-06 11:42:44 +0900

    design: 소켓 알림 디자인 수정

--- views/js/common.js
+++ views/js/common.js
@@ -3,7 +3,7 @@ const socket = io.connect('http://localhost:1004');
 
 socket.on('orderMessage', function (data) {
     const { customerId, customerName, driverId, driverName } = data;
-    const messageHtml = `${customerName}님이 방금 ${driverName} 차량을 이용 신청했어요!`;
+    const messageHtml = `<span style="font-weight:bold; font-size: 25px; color: blue;">${customerName}</span>님이 방금 <span style="font-weight:bold; font-size: 25px; color: blue;">${driverName}</span> 차량을 이용 신청했어요!`;
 
     const htmlTemp = `
         <div class="alert alert-primary show fade" role="alert" id="customerAlert">${messageHtml}
